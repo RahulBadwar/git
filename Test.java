3f60f8948e01fe2a79d7c0f5c21d9b121064955e
Rahul Badwar <rahulbadwar2014@gmail.com>
2022-05-27 17:54:41 +0530

    hhh

---
+++ Test.java
@@ -0,0 +1,8 @@
+class Test2{
+	public static void main(String... args){
+		System.out.println("GGGGGGG");
+		System.out.println("gggg");
+		System.out.println("yyy");
+		System.out.println("ggg");
+	}
+}
