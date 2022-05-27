817a583a598d103e770a6c972913dce79435d65d
RahulBadwar <100821578+RahulBadwar@users.noreply.github.com>
2022-05-27 22:41:37 +0530

    Delete Test2.java

--- Test2.java
+++
@@ -1,9 +0,0 @@
-class Test2 {
-	public static void main(String... args) {
-		System.out.println("GGGGGGG");
-		System.out.println("gggg");
-		System.out.println("yyy");
-		System.out.println("ggg");
-		System.out.println("changes made in mater 6.12");
-	}
-}
